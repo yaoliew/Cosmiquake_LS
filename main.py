0a7e9b640042762118ad4a45e6bc8bf266502215
getknomed <zyliew7@gmail.com>
2024-01-30 23:12:12 -0500

    All files in, processing and normalization up to actual model building.

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ X = X[~nan_mask]
 
 # Sequential model
 model = ks.models.Sequential([
-  layers.Dense(units=1)
+  layers.Dense(units=1, use_bias=True)
 ])
 
 # Loss and optimizer
@@ -62,52 +62,53 @@ model.compile(optimizer=optim, loss=loss)
 # Fit model
 history = model.fit(
   X, y,
-  epochs=35,
+  epochs=100,
   verbose=1,
   validation_split=0.2
 )
 
-print(model.evaluate(
-    test_features['Difference'],
-    test_labels['EQ_Sum'], verbose=1))
-
-def plot_loss(history):
-    plt.figure(figsize=(8, 6))
-    plt.plot(history.history['loss'], label='loss (training data)')
-    plt.plot(history.history['val_loss'], label='val_loss (validation data)')
-    plt.title('Loss for Earthquake Prediction Model')
-    plt.ylabel('Loss value')
-    plt.xlabel('No. epoch')
-    plt.legend(loc="upper right")
-    plt.show()
-
-plot_loss(history)
-plt.show()
-
-# plot line of best fit
-range_min = np.min(test_features['Difference']) - 10
-range_max = np.max(test_features['Difference']) + 10
-x_values = tf.linspace(range_min, range_max, 200)
-# Reshape input to 2D
-x_values = x_values[:,np.newaxis]
-y_pred = model.predict(x_values)
-
-# Convert the TensorFlow tensor to a NumPy array for plotting
-x_values = x_values.numpy().flatten()
-y_pred = y_pred.flatten()
-
-# Ensure that x_values and y_pred have the same dimensions
-assert len(x_values) == len(y_pred), "x and y must have the same length"
-
-# Plotting the predicted line
-plt.plot(x_values, y_pred, label='Predicted')
-
-# Plotting the actual data points
-plt.scatter(test_features['Difference'], test_labels['EQ_Sum'], color='red', label='Actual Data')
-
-# Adding labels and legend
-plt.xlabel('Difference')
-plt.ylabel('EQ_Sum')
-plt.legend()
-
-plt.show()
+#
+# print(model.evaluate(
+#     test_features['Difference'],
+#     test_labels['EQ_Sum'], verbose=1))
+#
+# def plot_loss(history):
+#     plt.figure(figsize=(8, 6))
+#     plt.plot(history.history['loss'], label='loss (training data)')
+#     plt.plot(history.history['val_loss'], label='val_loss (validation data)')
+#     plt.title('Loss for Earthquake Prediction Model')
+#     plt.ylabel('Loss value')
+#     plt.xlabel('No. epoch')
+#     plt.legend(loc="upper right")
+#     plt.show()
+#
+# plot_loss(history)
+# plt.show()
+#
+# # plot line of best fit
+# range_min = np.min(test_features['Difference']) - 10
+# range_max = np.max(test_features['Difference']) + 10
+# x_values = tf.linspace(range_min, range_max, 200)
+# # Reshape input to 2D
+# x_values = x_values[:,np.newaxis]
+# y_pred = model.predict(x_values)
+#
+# # Convert the TensorFlow tensor to a NumPy array for plotting
+# x_values = x_values.numpy().flatten()
+# y_pred = y_pred.flatten()
+#
+# # Ensure that x_values and y_pred have the same dimensions
+# assert len(x_values) == len(y_pred), "x and y must have the same length"
+#
+# # Plotting the predicted line
+# plt.plot(x_values, y_pred, label='Predicted')
+#
+# # Plotting the actual data points
+# plt.scatter(test_features['Difference'], test_labels['EQ_Sum'], color='red', label='Actual Data')
+#
+# # Adding labels and legend
+# plt.xlabel('Difference')
+# plt.ylabel('EQ_Sum')
+# plt.legend()
+#
+# plt.show()
